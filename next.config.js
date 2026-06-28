f3e482f45fde6130e73fb05ae1c38b6fc8c4d7a3
Emmanuel <shepherdaitimeless@gmail.com>
2026-06-28 12:39:48 -0400

    fix: Cross-Origin-Opener-Policy header to fix Google Sign-In popup on classifieds post

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
-
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
   disable: true,
 });
-
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -15,6 +13,22 @@ const nextConfig = {
       "lh3.googleusercontent.com",
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "unsafe-none",
+          },
+        ],
+      },
+    ];
+  },
 };
-
 module.exports = withPWA(nextConfig);
