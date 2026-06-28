24c446fb5010abca5234f48f9ed13098aae4d453
Emmanuel <shepherdaitimeless@gmail.com>
2026-06-28 15:17:27 -0400

    fix: COOP header to unsafe-none to stop blocking Google auth popup

--- next.config.js
+++ next.config.js
@@ -20,10 +20,6 @@ const nextConfig = {
         headers: [
           {
             key: "Cross-Origin-Opener-Policy",
-            value: "same-origin-allow-popups",
-          },
-          {
-            key: "Cross-Origin-Embedder-Policy",
             value: "unsafe-none",
           },
         ],
